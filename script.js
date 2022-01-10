23ed3a0c49c0a670a684ea8fd045b09ba86b09ca
Dujulacis <59266348+Dujulacis@users.noreply.github.com>
2022-01-10 09:36:21 +0200

    Add javascript update
    Values will now be printed in console.

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ function nolasa() {
     }
 }
 function perimeter (m1, m2, m3) {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
     const p = m1 + m2 + m3;
     console.log (p);
     return p;
 }
 function laukums (m1, m2, m3) {
-    const pusper = perimeter(m1, m2, m3)/2;
-    console.log(aba);
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    const pusper = (m1 + m2 + m3)/2;
     let laukums = Math.sqrt(pusper*(pusper-m1)*(pusper-m2)*(pusper-m3));
     console.log(laukums);
     return laukums;
